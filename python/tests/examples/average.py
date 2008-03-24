d1b3bc3dffb8ee3690dce306b55b67ddff82159c
Coda Hale <coda.hale@gmail.com>
2008-03-24 16:07:17 -0700

    Adding further complication to the averge example.

--- python/tests/examples/average.py
+++ python/tests/examples/average.py
@@ -9,20 +9,23 @@ sys.path.insert(0, 'lib')
 from collections import defaultdict
 from hadoop import Job
 from hadoop.parsers import TSVParser
+from hadoop.collectors import TSVCollector
 
 class AverageJob(Job):
   """Calculate the average value of a key/value TSV file."""
   def __init__(self):
     super(AverageJob, self).__init__()
     self.map_parser = TSVParser
+    self.map_collector = TSVCollector
+    self.reduce_parser = TSVParser
   
   def map(self, key, values, collector):
-    collector.collect(key, values[0])
+    collector.collect(key, values[0], 'yay')
   
   def reduce(self, iterator, collector):
     accumulator = defaultdict(list)
-    for word, count in iterator:
-      accumulator[word].append(int(count))
+    for word, values in iterator:
+      accumulator[word].append(int(values[0]))
     for word, counts in accumulator.iteritems():
       collector.collect(word, sum(counts) / float(len(counts)))
 
